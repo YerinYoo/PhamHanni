50db61b82622d8ea8d06ae10b5b574381c92271a
“YerinYoo” <ooyo.che1s@gmail.com>
2024-07-05 11:31:54 +0900

    이용권 리스트 띄우는 로직 > TicketController

--- newjeans/src/main/java/com/bunnies/infra/ticket/TicketController.java
+++ newjeans/src/main/java/com/bunnies/infra/ticket/TicketController.java
@@ -2,10 +2,23 @@ package com.bunnies.infra.ticket;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class TicketController {
 
 	@Autowired
 	TicketService service;
+	
+	//이용권 리스트 표출 페이지
+	@RequestMapping (value = "/Ticket-Plans")
+	public String TicketList (TicketDto dto, Model model) throws Exception {
+		
+		model.addAttribute("tickets", service.selectListWithoutPaging());
+		
+		return "ditto/plan";
+	}
+	
+	//이용권 결제 관련 로직 
 }
